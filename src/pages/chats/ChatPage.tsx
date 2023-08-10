b72e1eeddcb80dc998a2f51588018df45cba7702
makvkn <max.makovkin@yandex.ru>
2023-08-10 22:48:39 +0300

    Create default files and directories

---
+++ src/pages/chats/ChatPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import styles from './ChatPage.module.css';
+
+const ChatPage = () => {
+    return <div>{/*<h1 className={styles.lol}>hello from ChatPage</h1>*/}</div>;
+};
+
+export default ChatPage;
