a1d946e6b6d8f90b0e5ebc89f97fa710160dc3ca
maksim makovkin <maksim.makovkin@neuro-city.ru>
2023-08-21 16:23:50 +0300

    Navigation page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import ChatPage from './pages/chats/ChatPage';
 import styles from './App.module.css';
+import NavPage from './pages/navigation/NavPage';
 
 export const App = () => {
     return (
         <div className={styles.App}>
             <ChatPage />
+            <NavPage />
         </div>
     );
 };
---
+++ src/pages/navigation/NavPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavPage = () => {
+    return (
+        <div>
+            <a href="/lol">Chats</a>
+            <a href="/lol">My Account</a>
+            <a href="/lol">Settings</a>
+        </div>
+    );
+};
+
+export default NavPage;
